28d58d3b7d91754b3ef27d4e77ff6bd6b96d5251
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-13 15:53:52 -0500

    add: refactor ExperienceSection and ProjectsSection to use HomeSection component for consistency

---
+++ web/src/features/home/index.ts
@@ -0,0 +1,4 @@
+export { default as HeroSection } from './components/HeroSection.astro';
+export { default as ExperienceSection } from './components/ExperienceSection.astro';
+export { default as ProjectsSection } from './components/ProjectsSection.astro';
+export { default as AboutMeSection } from './components/AboutMeSection.astro';
