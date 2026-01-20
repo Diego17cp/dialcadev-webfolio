52ea8e9b71e1ad67229573238dbf7826cbd7aa7b
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-20 14:06:20 -0500

    add: integrate astro-robots-txt for improved SEO management

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,12 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
+  site: "https://dialcadev.vercel.app",
   integrations: [icon({
     include: {
       tabler: ["*"],
@@ -18,5 +20,5 @@ export default defineConfig({
       "devicon-plain": ["*"],
       "simple-icons": ["*"]
     }
-  })]
+  }), robotsTxt()]
 });
